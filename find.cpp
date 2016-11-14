05be89e059cb958c7e67371e515ab689038a8e21
Anshul Basia <anshul.dbgt@gmail.com>
2016-11-14 19:44:37 +0530

    fixed the bug

--- find.cpp
+++ find.cpp
@@ -42,7 +42,7 @@ bool KMP(string pattern, string target)
             i++;
             k++;
             if (k == m){
-            	to_find = i-k;
+                to_find = i-k;
                 return 1;
             }
         }
@@ -53,38 +53,37 @@ bool KMP(string pattern, string target)
 }
 
 int main(){
-	ifstream file("rishi.html");
-	string text;
-	cout<<"Enter the number of friends you want to see the name of"<<endl;
-	int n;
-	cin>>n;
-	string s = "InitialChatFriendsList";
-	int t;
-	string id;
-	vector<string> v;
-	while(getline(file,text,'\n')){
-		
-		if(KMP(s,text)){
-			t = to_find + 45;
-			while(v.size()<n){
-				id = "";
-				while(text[t]!='-'){
-					id = id + text[t];
-					t++;
-				}
-				cout<<id<<endl;
-				if(find(v.begin(),v.end(),id)==v.end()){
-					v.push_back(id);
-				}
-				t = t+ 5;
-			}
-			break;
-		}
-	}
+    ifstream file("rishi.html");
+    string text;
+    cout<<"Enter the number of friends you want to see the name of"<<endl;
+    int n;
+    cin>>n;
+    string s = "list:[";
+    int t;
+    string id;
+    vector<string> v;
+    while(getline(file,text,'\n')){
+        
+        if(KMP(s,text)){
+            t = to_find + 7;
+            while(v.size()<n){
+                id = "";
+                while(text[t]!='-'){
+                    id = id + text[t];
+                    t++;
+                }
+                if(find(v.begin(),v.end(),id)==v.end()){
+                    v.push_back(id);
+                }
+                t = t+ 5;
+            }
+            break;
+        }
+    }
 
-	for(int j=0;j<v.size();j++){
-		cout<<v[j]<<endl;
-		s = "firefox -new-tab http://www.facebook.com/"+v[j];
-		system(s.c_str());
-	}
+    for(int j=0;j<v.size();j++){
+        cout<<v[j]<<endl;
+        s = "firefox -new-tab http://www.facebook.com/"+v[j];
+        system(s.c_str());
+    }
 }
